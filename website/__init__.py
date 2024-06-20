8cf19cd9c8527d5b92f9390e3ab2bc495d158e6e
Talb057 <alitalbe33@gmail.com>
2024-06-20 11:58:26 -0400

    signup page

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import psycopg2
 
 def create_app():
     app = Flask(__name__)
@@ -11,3 +12,13 @@ def create_app():
     app.register_blueprint(auth, url_prefix = '/')
 
     return app
+
+def connect_db():
+    conn = psycopg2.connect(
+            host = "localhost",
+            database = "project_db",
+            user = "manager",
+            port = "5432",
+            password = "Mon12345")
+    
+    return conn
--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,54 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import psycopg2
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/signin')
 def signin():
     return render_template("signin.html")
+
+@auth.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        name = request.form.get("name")
+        
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute(f"SELECT user.id, user.email FROM user WHERE user.email = {email};")
+        data = cur.fetchall()
+        if(len(data) > 0):
+            # flash('Email already exists', category='error')
+            print("Email already exists")
+        elif len(password1) < 8:
+            # flash('Password must be longer than or 7 characters', category='error')
+            print("Passwrd must be longer than 8")
+        elif password1 != password2:
+            # flash('Passwords don\'t match', category='error')
+            print("Password don't match")
+        else:
+            cur.execute('INSERT INTO client (email, password, NAS, nom, addresse, card_number)'
+                'VALUES (%s, %s, %s, %s, %s, %s)',
+                (f'{name}',
+                f'{email}',
+                f'{password1}')
+                )
+            print("User added")
+            conn.commit()
+            cur.close() 
+            conn.close()
+
+
+    return render_template("signup.html")
+
+#Function to connect to the database
+def connect_db():
+    conn = psycopg2.connect(
+            host = "localhost",
+            database = "project_db",
+            user = "manager",
+            port = "5432",
+            password = "Mon12345")
+    return conn
