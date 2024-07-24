670fb6cb77b6acdc881ae0c41f75a76ef4febcda
Talb057 <alitalbe33@gmail.com>
2024-07-24 14:37:43 -0400

    updated home page

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_user, login_required, logout_user, current_user
+import psycopg2
 
 views = Blueprint('views', __name__)
 
@@ -10,4 +11,21 @@ def index():
 @views.route('/home')
 @login_required
 def home():
-    return render_template("home.html", user = current_user)
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM projects WHERE projects.user_id = '{current_user.id}';")
+    projects = cur.fetchall()
+    print(type(projects))
+    print(f"**************************************{projects}")
+    length = len(projects)
+    return render_template("home.html", user = current_user, projects = projects, length = length)
+
+#Function to connect to the database
+def connect_db():
+    conn = psycopg2.connect(
+            host = "localhost",
+            database = "project_db",
+            user = "postgres",
+            port = "5432",
+            password = "berserk")
+    return conn
